d81a869587bad11ff4381823ecb1961969be643a
francojposa <francojposa@gmail.com>
2020-03-21 11:08:08 -0500

    Defining API with not impleeneted handler as placeholder

--- main.go
+++ main.go
@@ -12,12 +12,21 @@ import (
 func main() {
 	router := mux.NewRouter()
 
+	// Setup Views & Static file handling on router
 	// On the default page we will simply serve our static index page.
 	router.Handle("/", http.FileServer(http.Dir("./views/")))
 	// We will setup our server so we can serve static assest like images, css from the /static/{file} route
 	router.PathPrefix("/static/").Handler(
 		http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))),
 	)
+
+	// Setup API handling on router
+	// /health - health check
+	router.Handle("/health", http.HandlerFunc(NotImplementedHandler)).Methods("GET")
+	// /locations - retrieve a list of We We locations a user can leave feedback on
+	router.Handle("/locations", http.HandlerFunc(NotImplementedHandler)).Methods("GET")
+	// /locations/{slug}/feedback - which will capture user feedback on locations
+	router.Handle("/locations/{slug}/feebdack", http.HandlerFunc(NotImplementedHandler)).Methods("POST")
 
 	srv := &http.Server{
 		Handler: router,
