e1aee274befa84962d2d6b29096fabc87f5571fe
francojposa <francojposa@gmail.com>
2020-03-28 15:25:23 -0500

    simplifying postgres URL building for now

--- oauth2-in-action/sql/psql.go
+++ oauth2-in-action/sql/psql.go
@@ -2,9 +2,6 @@ package sql
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
-	"time"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
@@ -12,64 +9,44 @@ import (
 
 // PostgresConfig defines Postgres SQL connection information
 type PostgresConfig struct {
-	ApplicationName string        // The name of the application connecting. Useful for attributing sql load.
-	Host            string        // The host where the database is located
-	Port            uint16        // The port on which the database is listening
-	Username        string        // The username for the database
-	Password        string        // The password for the database
-	Database        string        // The name of the database
-	ConnectTimeout  time.Duration // Amount of time to wait before timing out
+	Host                  string // The host where the database is located
+	Port                  uint16 // The port on which the database is listening
+	Username              string // The username for the database
+	Password              string // The password for the database
+	Database              string // The name of the database
+	ConnectTimeoutSeconds int    // Number of seconds to wait before timing out
 }
 
 // NewDefaultPostgresConfig creates and return a default postgres configuration.
 func NewDefaultPostgresConfig(appName, dbName string) PostgresConfig {
 	return PostgresConfig{
-		ApplicationName: appName,
-		Host:            "localhost",
-		Port:            5432,
-		Database:        dbName,
-		ConnectTimeout:  5 * time.Second,
+		Host:                  "localhost",
+		Port:                  5432,
+		Database:              dbName,
+		ConnectTimeoutSeconds: 5,
 	}
 }
 
 // buildConnectionString transforms the PostgresConfig into a usable connection string for lib/pq.
 // If a missing or invalid field is provided, an error is returned.
-func buildConnectionString(pc PostgresConfig) (string, error) {
-	if pc.Database == "" {
-		return "", fmt.Errorf("postgres database name was not specified")
-	}
-	if pc.ApplicationName == "" {
-		return "", fmt.Errorf("application name must be specified to connect to postgres")
-	}
+func buildConnectionString(pc PostgresConfig) string {
+
 	auth := ""
 	if pc.Username != "" || pc.Password != "" {
 		auth = fmt.Sprintf("%s:%s@", pc.Username, pc.Password)
 	}
 	url := fmt.Sprintf(
-		"postgres://%s%s:%d/%s",
+		"postgres://%s%s:%d/%s?connect_timeout=%d&sslmode=disable",
 		auth,
 		pc.Host,
 		pc.Port,
 		pc.Database,
+		pc.ConnectTimeoutSeconds,
 	)
-	options := []string{fmt.Sprintf("application_name=%s", pc.ApplicationName)}
-
-	if pc.ConnectTimeout.Seconds() > 0 {
-		timeoutStr := strconv.Itoa(int(pc.ConnectTimeout.Seconds()))
-		options = append(options, fmt.Sprintf("connect_timeout=%s", timeoutStr))
-	}
-
-	//TODO figure out SSL
-	options = append(options, "sslmode=disable")
-
-	return fmt.Sprintf("%s?%s", url, strings.Join(options, "&")), nil
+	return url
 }
 
 func MustConnect(pc PostgresConfig) *sqlx.DB {
-	pgURL, err := buildConnectionString(pc)
-	if err != nil {
-		panic(err)
-	}
-
+	pgURL := buildConnectionString(pc)
 	return sqlx.MustConnect("postgres", pgURL)
 }
